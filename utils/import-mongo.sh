fac5ae4fd1531b0efd0e6473c19b9faeed7b3ddc
andres123dbh <andres123dbh@gmail.com>
2023-02-28 22:13:00 -0500

    Setup mongo service, mongo express and import data from json

---
+++ utils/import-mongo.sh
@@ -0,0 +1,8 @@
+mongoimport \
+	--db 'movil_parcial' \
+	--collection 'users' \
+	--file '/data/users.json' \
+	--jsonArray \
+	--uri "mongodb://root:dev@mongo:27017" \
+	--authenticationDatabase 'admin'
+
